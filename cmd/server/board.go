702cf5ea75b710d59932d64d26ca9108082085a7
Akilesh Sethu <asethu19@gmail.com>
2021-10-28 17:56:27 +0800

    more comments

--- cmd/server/board.go
+++ cmd/server/board.go
@@ -7,7 +7,8 @@ import (
 )
 
 func (app *application) CreateBoardHandler(ctx context.Context, boardRequest *pb.BoardRequest) (*pb.Board, error) {
-	boardResult := <-app.models.Board.InsertAsync(ctx, boardRequest.Name, boardRequest.Description, boardRequest.MemberIds)
+	boardResult, _ := <-app.models.Board.InsertAsync(ctx, boardRequest.Name, boardRequest.Description, boardRequest.MemberIds)
+	//do you use the false from channel alone to determine if its closed and throw an error or explicitly get an error from request as in comment in InsertAsync
 	if boardResult.Err != nil {
 		return nil, boardResult.Err
 	}
